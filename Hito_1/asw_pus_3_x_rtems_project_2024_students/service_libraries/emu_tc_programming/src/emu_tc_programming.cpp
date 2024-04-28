a4ab904a676ccdef542ffe6b49b7522b9d705ced
atcsol <estrefugaz12@gmail.com>
2024-04-28 15:46:21 +0200

    Añadida prueba 0060 de PUS 3

--- Hito_1/asw_pus_3_x_rtems_project_2024_students/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
+++ Hito_1/asw_pus_3_x_rtems_project_2024_students/service_libraries/emu_tc_programming/src/emu_tc_programming.cpp
@@ -15,10 +15,10 @@
 //#define FT_SOLO_EPD_ICU_SERV_9_0020
 //#define FT_SOLO_EPD_ICU_SERV_1_REJECTED_TC_0030
 
-// Service 3 tests
+// Service 3 tests.
 //#define FT_SOLO_EPD_ICU_SERV_3_DISABLE_SID0_0040
-#define FT_SOLO_EPD_ICU_SERV_3_UPDATE_INTERVAL_TC_0050
-//...
+//#define FT_SOLO_EPD_ICU_SERV_3_UPDATE_INTERVAL_TC_0050
+#define FT_SOLO_EPD_ICU_SERV_3_MAX_FREQUENCY_0060
 
 
 #ifdef FT_SOLO_EPD_ICU_SERV_17_0010
@@ -64,5 +64,15 @@ EmuGSS_TCProgram3_5 prog_FT_0040_step_1(UNITIME_AFTER_POWER_ON + 20,
 
 EmuGSS_TCProgram3_31 prog_FT_0050_step_0(UNITIME_AFTER_POWER_ON + 10,
 		"FT_SOLO_EPD_ICU_SERV_3_0050 Step 0, Cambia periodo de SID=0 a 5", 0, 5);
+
+#endif
+
+
+#ifdef FT_SOLO_EPD_ICU_SERV_3_MAX_FREQUENCY_0060
+
+EmuGSS_TCProgram3_31 prog_FT_0060_step_0(UNITIME_AFTER_POWER_ON + 4,
+		"FT_SOLO_EPD_ICU_SERV_3_0060 Step 0, Cambia periodo de SID=0 a 1", 0, 1);
+EmuGSS_TCProgram3_31 prog_FT_0060_step_1(UNITIME_AFTER_POWER_ON + 8,
+		"FT_SOLO_EPD_ICU_SERV_3_0060 Step 1, Cambia periodo de SID=10 a 0", 10, 0);
 
 #endif
